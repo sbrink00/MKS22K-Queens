64c26b1cc08e0c4cbcee1ef66487359e9d586919
sbrink00 <sbrink00@stuy.edu>
2019-02-07 09:18:09 -0500

    created file and methods and started writing addQueen

---
+++ QueenBoard.java
@@ -0,0 +1,31 @@
+public class QueenBoard{
+  public static void main(String[]args){
+    QueenBoard q = new QueenBoard(5);
+  }
+
+  private int[][] board;
+
+  public QueenBoard(int size){
+    board = new int[size][size];
+  }
+
+  private boolean addQueen(int r, int c){
+    board[r][c] = -1
+  }
+
+  private boolean removeQueen(int r, int c){}
+
+  public boolean solve(){
+    for (int col = 0; col < board[0].length; col ++){
+      
+    }
+  }
+
+  public int countSolutions(){}
+
+  public String toString()
+
+
+
+
+}
